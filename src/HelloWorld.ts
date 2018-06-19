89ecf872146dce6a716f3f6dbcdbfacd84619811
hoonboon <hongwen77@gmail.com>
2018-06-19 15:11:26 +0800

    ..

--- src/HelloWorld.ts
+++ src/HelloWorld.ts
@@ -1,6 +1,40 @@
+let z: number = 100;
+
+function pluck<T, K extends keyof T>(o: T, names: K[]): T[K][] {
+    return names.map(n => o[n]);
+}
+
+interface Person {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+let person: Person = {
+    name: 'Some Body',
+    age: 33,
+    gender: 'm'
+};
+
+let strings: string[] = pluck(person, ['name', 'gender']);
+
 export class HelloWorld {
+    protected prop01: string;
+    
+    constructor(inputStr: string){
+        this.prop01 = inputStr;
+    };
+
     public helloWorld(): string {
-        return 'Hello World!';
+        z = 110; // mocha test: calling this function before addToZ() will cause function test to fail
+        return `Hello World! ${this.prop01}`;
+    }
+
+    public addToZ(x: number, y: number): number {
+        console.log(`strings: ${JSON.stringify(strings)}`);
+        console.log(`person.name: ${person.name}`);
+        console.log(`person[name]: ${person["name"]}`);
+        return x + y + z;
     }
 }
 
--- test/HelloWorld.test.ts
+++ test/HelloWorld.test.ts
@@ -6,8 +6,16 @@ should();
 describe('Hello World test suite', function() {
     it('Can say Hello World', function(done) {
         const expected = 'Hello World!';
-        const result = new HelloWorld().helloWorld();
+        const result = new HelloWorld("").helloWorld();
+        console.log(`Returned: ${result}`);
         result.should.equals(expected, `Should return: ${expected}, but returned: ${result}`);
         done();
+    });
+
+    it('Can add to Z', function() {
+        const expected = 150;
+        const result = new HelloWorld("").addToZ(20, 30);
+        console.log(`Returned: ${result}`);
+        result.should.equals(expected, `Should return: ${expected}, but returned: ${result}`);
     });
 });
